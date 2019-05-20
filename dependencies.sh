eaac26a9b120b4393b483524b5279f4b983df51a
Bob Lantz <rlantz@cs.stanford.edu>
2019-05-20 14:00:00 -0700

    Add and check for fuser/psmisc dependency

--- dependencies.sh
+++ dependencies.sh
@@ -5,7 +5,7 @@ PIP3='pip3 -q'
 APT='apt -qq -y'
 
 echo "* Installing python dependencies"
-  sudo $APT install python3-pip python3-setuptools python3-wheel
+  sudo $APT install python3-pip python3-setuptools python3-wheel psmisc
   $PIP3 install protobuf grpcio grpcio-tools requests prometheus_client
 
 echo "* Done"
