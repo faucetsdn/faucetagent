8a6596404344301a490d787be41ad1a3ae9e3a46
Josh Bailey <josh@vandervecken.com>
2020-06-10 16:15:48 +1200

    flake8.

--- agenttest.py
+++ agenttest.py
@@ -368,8 +368,8 @@ def end_to_end_test(cert_dir=CERT_DIR,
     info('* Shutting down any agents listening on %d\n' % GNMI_PORT)
     kill_server(port=gnmi_port)
 
-    cfile = os.path.join(
-        cdir, 'faucet.yaml')  # pylint: disable=possibly-unused-variable
+    cfile = os.path.join(  # pylint: disable=possibly-unused-variable
+        cdir, 'faucet.yaml')
 
     info('* Starting agent\n')
     nohup = '--nohup' if nohup else ''
